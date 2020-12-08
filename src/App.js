0ccded893eb74b22c441919e8c8c406458f455bc
Tyler Farhner <tylerfarhner@gmail.com>
2020-12-08 14:44:47 -0800

    adds sign up form

--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ import Footer from './components/Footer/Footer'
 
 // ------Page Imports----------------
 import HomePage from './pages/HomePage'
-import Dashboard from './pages/DashboardPage'
+import DashboardPage from './pages/DashboardPage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+
+// ------switch and Route import-----
+import { Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <HomePage />
-      <Dashboard />
-      <LoginPage />
-      <SignupPage />
-      <Footer />
+    <Header/>
+      <Switch>
+        <Route exact path ='/' render={props=>
+          <HomePage/>
+        }/>
+        <Route exact path ='/dashboard' render={props=>
+          <DashboardPage/>
+        }/>
+        <Route exact path ='/login' render={props=>
+          <LoginPage/>
+        }/>
+        <Route exact path ='/signup' render={props=>
+          <SignupPage/>
+        }/>
+      </Switch>
+    <Footer/>
     </div>
   );
 }
