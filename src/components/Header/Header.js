2813a7f2909c02edc5279c861ee3c307cfb24cf9
Tyler Farhner <tylerfarhner@gmail.com>
2020-12-08 12:24:25 -0800

    sets up layout w styles

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import './Header.css'
+
+function Header(props) {
+    return (
+        <header className="Header">
+            <h1>My Project</h1>
+            <nav>
+                <ul className="NavLinks">
+                    <li>Logout</li>
+                    <li>Dashboard</li>
+                    <li>Signup</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
